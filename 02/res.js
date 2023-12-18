f43a6a416b63cc7864c5ec6155faa1eeddb9372d
CMR211 <bartosz.surma211@gmail.com>
2023-12-18 16:04:54 +0100

    day 02 init

---
+++ 02/res.js
@@ -0,0 +1,9 @@
+import { readInputFileContents } from "../utils/readInputFileContents.js"
+const input = readInputFileContents("02/input.txt").split("\n")
+
+const MAX = {
+    "RED": 12,
+    "GREEN": 13,
+    "BLUE": 14
+}
+
